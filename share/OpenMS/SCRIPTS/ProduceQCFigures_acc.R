528dfc4d0ae18890b197ea8ba3ebb4e637e66ab5
mwalzer <walzer@informatik.uni-tuebingen.de>
2015-01-19 13:28:28 +0100

    [NOP] qc plots script adaption for better knime-R/cmdl-R compatibility

--- share/OpenMS/SCRIPTS/ProduceQCFigures_acc.R
+++ share/OpenMS/SCRIPTS/ProduceQCFigures_acc.R
@@ -1,24 +1,16 @@
-ls ## This is an R script to produce the figures that are attached to the qcML format
+## This is an R script to produce the figures that are attached to the qcML format
 
 #options
 options(digits=10)
 
-file<-commandArgs(TRUE)[1]
-post<-commandArgs(TRUE)[2]
+ids<-commandArgs(TRUE)[1]
 ##########################
 ###Mass accuracy
 ##########################
-a<-read.delim(file)
-png(post)
-if ('delta_ppm' %in% colnames(a)) {
-	hist(a$delta_ppm,xlim=c(-10,10),breaks=seq(min(a$delta_ppm)-0.01, max(a$delta_ppm)+0.01, 0.01),xlab="ppm",main=paste("delta ppm"))
-	abline(v=median(a$delta_ppm),col="red", lwd=2)
-	mtext(paste("median(accuracy)=",round(median(a$delta_ppm),3)," ppm",sep=""))
-} else {
-	tmp<-(1-a$MZ/a$TheoreticalWeight)*1e6
-	hist(tmp,xlim=c(-10,10),breaks=seq(min(tmp)-0.01, max(tmp)+0.01, 0.01),xlab="ppm",main=paste("delta ppm"))
-	abline(v=median(tmp),col="red", lwd=2)
-	mtext(paste("median(accuracy)=",round(median(tmp),3)," ppm",sep=""))
-}
+knime.in <- read.table(file=ids, header=TRUE, sep="\t", na.strings="?", dec=".", strip.white=TRUE)
+names(knime.in) <- c('RT','MZ', 'Score', 'PeptideSequence', 'Charge', 'TheoreticalWeight', 'DeltaPpm', 'Oxidation(M)')
+hist(knime.in$"DeltaPpm",xlim=c(-10,10),breaks=seq(min(knime.in$"DeltaPpm")-0.01, max(knime.in$"DeltaPpm")+0.01, 0.01),xlab="ppm", main="")
+abline(v=median(knime.in$"DeltaPpm"),col="red", lwd=2)
+mtext(paste("median(accuracy)=",round(median(knime.in$"DeltaPpm"),3)," ppm",sep="")
 ######################################
 dev.off()
--- share/OpenMS/SCRIPTS/ProduceQCFigures_idmap.R
+++ share/OpenMS/SCRIPTS/ProduceQCFigures_idmap.R
@@ -6,19 +6,17 @@ options(digits=10)
 ##########################
 ###IDs on rt/mz map vs precursors
 ##########################
-file_p<-read.table(commandArgs(TRUE)[1], header=TRUE, sep="", na.strings="NA", dec=".", strip.white=TRUE)
-file_id<-read.table(commandArgs(TRUE)[2], header=TRUE, sep="", na.strings="NA", dec=".", strip.white=TRUE)
-post<-commandArgs(TRUE)[3]
 
-file_p$RT_.sec. = file_p$RT_.sec./60
-file_id$RT = file_id$RT/60
-
-spec<-cbind(file_p,col=rep(1,length(file_p$RT_.sec.)))
-id<-cbind(file_id[,1:2],rep(2,length(file_id$RT)))
-all<-rbind(as.matrix(spec),as.matrix(id))
-
-png(post)
-plot(all,col=all[,3], xlab="RT[sec]", ylab="mz", pch=4, cex=0.3)
+ids <- read.table("/tmp/ids.csv", header=TRUE, sep="\t", na.strings="?", dec=".", strip.white=TRUE)
+ms2s <- read.table("/tmp/ms2s.csv", header=TRUE, sep="\t", na.strings="?", dec=".", strip.white=TRUE)
+names(ms2s) <- c('RT','Precursor')
+#names(ids) <- c('RT','MZ', 'Score', 'PeptideSequence', 'Charge', 'TheoreticalWeight', 'DeltaPpm')
+knime.in<-merge(x = ms2s, y = ids, by.x = "RT", by.y = "RT",  all=TRUE)
+recorded<-knime.in[knime.in$Precursor != "?", ]
+plot(recorded$RT/60,recorded$Precursor,pch=16,,xlab="RT (min)",ylab="m/z",cex=0.3)
+identified<-knime.in[knime.in$MZ != "?", ]
+points(identified$RT/60,identified$MZ,col="red",pch=4,cex=0.3)
 legend("topleft",c("recorded spectra","identified spectra"),pch=19,col=c(1,2))
+
 ######################################
 dev.off()
